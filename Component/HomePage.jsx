268e9e49e67599d8800361201a910787e5924cdc
romanbr87 <75772227+romanbr87@users.noreply.github.com>
2021-01-24 17:04:35 +0200

    Add files via upload

--- Component/HomePage.jsx
+++ Component/HomePage.jsx
@@ -7,12 +7,12 @@ import '../style.css';
 export default function HomePage(props) {
 
     return (
-        <div style={{ backgroundColor: 'rgb(84,166,189,68%)', border: '5px solid rgb(221,212,220)',borderRadius:'25px', marginLeft: '20%', marginRight: '20%', color: 'black' }}>
-           {/* gets the elemets and index from the App and retuns box-room Color\Name\Type */}
-            { props.rooms.map((e, i) => {
-                return <h2 style={{ backgroundColor: e.color, border: '5px solid rgb(255,255,255,57%)',borderRadius:'25px', textAlign: 'center', margin: '10px', padding: '5px', display: "inline-block", width: '160px' }}>
-                    <Link to="/Room" onClick={ () => props.getRoom(i)} >
-                        {e.roomType} <hr />
+        <div className='boxShadow' style={{ backgroundColor: 'rgb(84,166,189,68%)', border: '5px solid rgb(221,212,220)', borderRadius: '25px', paddingTop: '10px', marginLeft: '20%', marginRight: '20%', color: 'black' }}>
+            {/* gets the elemets and index from the App and retuns box-room Color\Name\Type */}
+            {props.rooms.map((e, i) => {
+                return <h2 className='boxShadow' style={{ backgroundColor: e.color, border: '5px solid rgb(255,255,255,57%)', borderRadius: '25px', textAlign: 'center', margin: '10px', padding: '5px', display: "inline-block", width: '160px' }}>
+                    <Link to="/Room" onClick={() => props.getRoom(i)} >
+                        {e.roomType} <hr style={{ width: '85%' }} />
                         {e.name}
                     </Link> <br />
                     <img src={deleteIcon} className="delButton" onClick={() => props.del(i)} />
@@ -20,7 +20,7 @@ export default function HomePage(props) {
             })
             }
             <br /><br />
-            <Link to="/AddRoom"><h3 style={{ backgroundColor: '#8ebdd8', border: '5px solid rgb(221,212,220)', borderRadius:'10px', padding:'5px', color: 'black', display: 'inline-flex' }}> Create a new Room </h3></Link>
+            <Link to="/AddRoom"><h3 className='boxShadow' style={{ backgroundColor: '#8ebdd8', border: '5px solid rgb(221,212,220)', borderRadius: '10px', padding: '5px', color: 'black', display: 'inline-flex' }}> Create a new Room </h3></Link>
         </div>
     )
 }
--- Component/Room.jsx
+++ Component/Room.jsx
@@ -8,11 +8,11 @@ export default function Room(props) {
     const [show, setShow] = useState(false)
     // swicth the device component true/false (on/off)
     const showProductsForm = (state) => {
-        setShow (state)
-    } 
+        setShow(state)
+    }
 
     return (
-        <div style={{ backgroundColor: props.room.color, border: '5px solid rgb(221,212,220)',borderRadius:'25px', marginLeft: '20%', marginRight: '20%', color: 'black' }}>
+        <div className='boxShadow' style={{ backgroundColor: props.room.color, border: '5px solid rgb(221,212,220)', borderRadius: '25px', marginLeft: '20%', marginRight: '20%', color: 'black' }}>
 
             <h2 style={{ margin: '10px', padding: '5px', textAlign: 'left' }}>
                 name: {props.room.name}
@@ -21,24 +21,24 @@ export default function Room(props) {
                 <hr />
             </h2>
 
-            <h2 style={{textDecorationLine: 'underline'}}>My device list</h2>
-            { 
+            <h2 style={{ textDecorationLine: 'underline' }}>My device list</h2>
+            {
                 props.room.products.map((e, i) => {
-                return ( 
-                    // ? is Trinary operation- that sets a condition if red or green
-                 <h3 className="product" style={{ backgroundColor: e.isTurnedOn ? 'green': 'red'  }}
-                 onClick={()=> props.swithchDevice(i) }>
-                 { e.name }</h3>
-                )
+                    return (
+                        // ? is Trinary operation- that sets a condition if red or green
+                        <h3 className='boxShadow' className="product" style={{ backgroundColor: e.isTurnedOn ? 'green' : 'red' }}
+                            onClick={() => props.swithchDevice(i)}>
+                            {e.name}</h3>
+                    )
                 })
             }
-            
-            <br /><hr style={{margin:'15px'}}/>
-            {/* Button that opens the option of adding a device- show the block or dont show at all */}
-            <button style={{borderRadius:'10px', margin:'auto',marginBottom:'8px', display: show ? 'none':'block' }} 
-            onClick={()=> showProductsForm (true)}>Add product</button>
 
-            { show? <Product room={props.room} setShow={showProductsForm} setDevices={props.addDevice}>
+            <br /><hr style={{ margin: '15px' }} />
+            {/* Button that opens the option of adding a device- show the block or dont show at all */}
+            <button className='boxShadow' style={{ cursor: 'pointer', borderRadius: '10px', margin: 'auto', marginBottom: '15px', display: show ? 'none' : 'block' }}
+                onClick={() => showProductsForm(true)}>Add product</button>
+
+            {show ? <Product room={props.room} setShow={showProductsForm} setDevices={props.addDevice}>
             </Product> : ''}
         </div>
     )
